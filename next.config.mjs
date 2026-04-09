cafaa4455215d805ab6654175971794f86cc658e
ambruchan <ambre.barah@epitech.eu>
2026-04-09 12:16:28 +0200

    TestingWebApp

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const repoName = process.env.GITHUB_REPOSITORY_NAME || "";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: isGithubPages && repoName ? `/${repoName}` : "",
+  assetPrefix: isGithubPages && repoName ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
